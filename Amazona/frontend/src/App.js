4cd85cf421f6502d4a695e3f198f39a619a95735
viraj <hemanthaviraj6@gmail.com>
2024-02-08 10:34:45 +0530

    first commit

---
+++ Amazona/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Amazona</a>
+      </header>
+       <main>
+        list Products
+       </main>
+    </div>
+  );
+}
+
+export default App;
